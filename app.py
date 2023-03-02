ac97a98c7b13e6117431bf733bb75a4f2412592c
Monsalvo Geoffery <monsalvo.geoffrey@gmail.com>
2023-03-02 18:39:03 +0100

    Base of 7-1 pattern

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
+from flask import Flask, request, jsonify, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+
+
+
+# from conversation import Conversation
+
+
+
+# def main():
+#     conv = Conversation()
+#     while True:
+#         message = input("You: ")
+#         response = conv.prompt(message)
+#         print(f"Assistant: {response}")
+
+# if __name__ == "__main__":
+#     main()
