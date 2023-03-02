785141fe5c9f70bc8f1ded6cdcc4d40a559193d8
Monsalvo Geoffery <monsalvo.geoffrey@gmail.com>
2023-03-02 18:32:22 +0100

    Initial working conversation bot in CLI

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from conversation import Conversation
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
+from conversation import Conversation
 
-key = os.environ.get('OPEN_AI_KEY')
 
-print(key)
+
+def main():
+    conv = Conversation()
+    while True:
+        message = input("You: ")
+        response = conv.prompt(message)
+        print(f"Assistant: {response}")
+
+if __name__ == "__main__":
+    main()
